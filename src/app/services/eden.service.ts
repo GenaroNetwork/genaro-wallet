d1bcbca4e6be1b3efd9e2da8730bcfb90bf9e2c9
孟子易 <mengziyi540841@gmail.com>
2018-07-06 14:55:44 +0800

    service

--- src/app/services/eden.service.ts
+++ src/app/services/eden.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EdenService {
 
-constructor() { }
+  // 当前路径，用于更新界面
+  currentPath: BehaviorSubject<string[]> = new BehaviorSubject([""]);
+
+  // 当前视图，用于更新界面
+  currentDisplay: BehaviorSubject<any[]> = new BehaviorSubject([]);
+  /**   demo: 如下参数构成的数组
+   *    {
+   *      name: "test",  // 文件名，如果以 / 结尾，则删除斜杠，标记为文件夹
+   *      type: "file", "bucket", "folder" 三者之一
+   *      
+   *      
+   *    }
+   */
+  constructor() {
+  }
 
 }
