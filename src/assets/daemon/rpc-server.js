c4d26f806628661727e22ca9cea8428a7461457a
孟子易 <mengziyi540841@gmail.com>
2018-07-31 15:46:05 +0800

    fix multiple bitcore-lib error

--- src/assets/daemon/rpc-server.js
+++ src/assets/daemon/rpc-server.js
@@ -1,16 +1,26 @@
 #!/usr/bin/env node
 
 'use strict'
+Object.defineProperty(global, "_bitcore", {
+  get() {
+    return void 0
+  },
+  set() {},
+});
 const genaroshare = require('genaroshare-daemon');
 const dnode = require('dnode');
 let api = new genaroshare.RPC();
 
 dnode(api.methods).listen(process.env.RPC_PORT, () => {
   console.log('listening..');
-  process.send({state: 'init'});
+  process.send({
+    state: 'init'
+  });
 });
 
 process.on('uncaughtException', (err) => {
   Object.assign({}, err);
-  process.send({error: err.stack}); // 'A Fatal Exception has occured in the genaroshare-daemon RPC server'
+  process.send({
+    error: err.stack
+  }); // 'A Fatal Exception has occured in the genaroshare-daemon RPC server'
 });
