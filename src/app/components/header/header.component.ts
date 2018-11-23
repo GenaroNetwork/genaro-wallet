b4a90c75b1fae2f5ad530d90822504b9643f1c3c
KoanSun <sunlei_0104@sina.cn>
2018-11-23 11:46:57 +0800

    update

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { TransactionService } from '../../services/transaction.service';
 import { WalletService } from '../../services/wallet.service';
 import { SettingService } from '../../services/setting.service';
+import { EdenService } from '../../services/eden.service';
+import { setNetType } from '../../libs/config';
 
 @Component({
   selector: 'app-header',
@@ -17,6 +19,7 @@ export class HeaderComponent implements OnInit {
     public txService: TransactionService, // 会在 html 中用到，
     public walletService: WalletService, // 会在 html 中用到，
     public setting: SettingService,
+    public edenService: EdenService,
   ) { }
 
   ngOnInit() {
@@ -33,11 +36,8 @@ export class HeaderComponent implements OnInit {
 
   setNet(netType) {
     this.netType = netType;
-    if (netType === 'main') {
-
-    }
-    else if (netType === 'test') {
-
-    }
+    setNetType(netType);
+    this.txService.connect();
+    this.edenService.resetAll();
   }
 }
