097d3869571e8c9bd79448bd81553bed3dfe294b
KoanSun <sunlei_0104@sina.cn>
2018-12-24 17:35:26 +0800

    update

--- src/app/services/eden.service.ts
+++ src/app/services/eden.service.ts
@@ -806,7 +806,6 @@ export class EdenService {
   async downloadMessage(file, bucketId, filePath) {
     return new Promise(async (res, rej) => {
       try {
-        debugger;
         let walletAddr = this.walletService.wallets.current;
         const env = this.allEnvs[walletAddr];
         walletAddr = await this.txService.add0x(walletAddr);
